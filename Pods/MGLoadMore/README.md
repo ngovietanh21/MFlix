1f48067e54d5a44a788e7d4b0dadd3e4f82ba451
ngovietanh21 <ngovietanh21@gmail.com>
2021-03-27 22:42:31 +0700

    Remove pods

--- Pods/MGLoadMore/README.md
+++
@@ -1,2 +0,0 @@
-# MGLoadMore
-LoadMoreTableView, LoadMoreCollectionView
--- Pods/Target Support Files/Pods-MFlixTests/Pods-MFlixTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MFlixTests : NSObject
-@end
-@implementation PodsDummy_Pods_MFlixTests
-@end
